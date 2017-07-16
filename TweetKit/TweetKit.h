bc80c2c3d675b92894dcc8f7582a5f0d0c01de6a
Bharat Byan <bharat.byan@gmail.com>
2017-07-16 18:31:51 +0530

    -First Commit again.

---
+++ TweetKit/TweetKit.h
@@ -0,0 +1,19 @@
+//
+//  TweetKit.h
+//  TweetKit
+//
+//  Created by Bharat Byan on 16/07/17.
+//  Copyright © 2017 Bharat Byan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TweetKit.
+FOUNDATION_EXPORT double TweetKitVersionNumber;
+
+//! Project version string for TweetKit.
+FOUNDATION_EXPORT const unsigned char TweetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TweetKit/PublicHeader.h>
+
+
